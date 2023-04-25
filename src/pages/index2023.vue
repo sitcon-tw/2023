251e5507e3ce8efa33599c2ecd431e969e8f3153
Aven910126 <90101934+Aven910126@users.noreply.github.com>
2023-04-25 19:44:27 +0800

    Update index2023.vue

--- src/pages/index2023.vue
+++ src/pages/index2023.vue
@@ -14,7 +14,7 @@
       <div id="first-section">
         <div class="fade-in">
           <div class="mainlogo">
-            <img :src="'imgs/2023/SITCON&GDSC_logo_BIG.svg' alt />
+            <img :src="'imgs/2023/SITCON&GDSC_logo_BIG.svg'" alt />
           </div>
           <div class="fade-in" id="first-section-title">學生社群大亂鬥</div>
           <div class="datefont">2023 07/29~07/30 國立臺灣科技大學</div>
