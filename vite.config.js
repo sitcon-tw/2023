55ccedfdefa920c0db683f22e37881ffa62fba78
fan9704 <cxz123499@gmail.com>
2023-04-26 15:16:26 +0800

    Unplug Vuetify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url'
 import vue from '@vitejs/plugin-vue'
-import vuetify from '@vuetify/vite-plugin'
+// import vuetify from '@vuetify/vite-plugin'
 import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
